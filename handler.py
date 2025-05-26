de3861f8eda09948085614877ad5216f161350a1
curlyfunk <dimitrifromsofia@gmail.com>
2025-05-26 12:36:22 +0300

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,11 +1,6 @@
 import runpod
-from inference import run_musetalk
 
 def handler(event):
-    input_data = event["input"]
-    audio_b64 = input_data.get("audio")
-    video_b64 = input_data.get("video")
-    result = run_musetalk(audio_b64, video_b64)
-    return {"output": result}
+    return {"output": "OK"}
 
 runpod.serverless.start({"handler": handler})
